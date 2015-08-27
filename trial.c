23d12d5a7ace464d7fc1c5101c150d06768bf087
Sally Lehman <sxlehman@godaddy.com>
2015-08-26 19:56:53 -0700

    farenheit program

--- trial.c
+++ trial.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+/* print Fahrenheit-Celsius table */
 main()
 {
-    printf("hello, world\n");
+    int fahr, celcius;
+    int lwer, upper, step;
+
+    lower = 0;    /* lower limit of temp table */
+    upper = 300;  /* upper limit */
+    step = 20;    /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
 }
