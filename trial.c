80445fc91092098b891ef4900c47e27b586e56dd
Sally Lehman <sxlehman@godaddy.com>
2015-08-26 20:58:06 -0700

    floaty

--- trial.c
+++ trial.c
@@ -3,7 +3,7 @@
 /* print Fahrenheit-Celsius table */
 main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;    /* lower limit of temp table */
@@ -12,8 +12,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
