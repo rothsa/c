f5a39c35f263813bae92e08e7b3af50081968b6f
Sally Lehman <sxlehman@godaddy.com>
2015-08-30 15:32:58 -0700

    add #define

--- trial.c
+++ trial.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0   /* lower limit of table */
+#define UPPER 300 /* upper limit of table */
+#define STEP  20  /* step size */
 /* print Fahrenheit-Celsius table */
 main()
 {
-    float fahr, celsius;
-    int lower, upper, step;
+    int fahr;
 
-    lower = 0;    /* lower limit of temp table */
-    upper = 300;  /* upper limit */
-    step = 20;    /* step size */
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
 
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+        printf("%3.0f %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
